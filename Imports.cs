ecc9907086cb6393199950a0ca606295cffc9e61
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-08-04 15:13:52 +0300

    Add project files.

---
+++ Imports.cs
@@ -0,0 +1,8 @@
+// Markup FmgLib
+global using FmgLib.MauiMarkup;
+global using MatchingImage;
+global using static Microsoft.Maui.Controls.FontAttributes;
+global using static Microsoft.Maui.Controls.LayoutAlignment;
+// Static
+global using static Microsoft.Maui.Graphics.Colors;
+global using MC = Microsoft.Maui.Controls;
